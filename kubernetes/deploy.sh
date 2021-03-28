e37a46835222c84ee48aa607ceaa91699c4a94c9
Soumya Ranjan Rout <soumyarout80@gmail.com>
2021-03-28 11:46:37 +0530

    Added namespace apply command

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -1,3 +1,4 @@
-kubectl -n openstad apply -f ingress.yml 
+kubectl -n openstad apply -f ingress.yml
+kubectl -n openstad apply -f namespace.yml
 kubectl -n openstad apply -f deployment.yml
 kubectl -n openstad apply -f service.yml
